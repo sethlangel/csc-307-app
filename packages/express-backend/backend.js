82e654075535e05faac56795c5718154c64ef570
sethlangel <seth@blueplate.ai>
2025-04-07 11:24:19 -0700

    Adds detailed user search and deletion of users.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -42,20 +42,47 @@ const findUserByName = (name) => {
     );
   };
 
+  const findUserByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name && user["job"] === job
+    );
+  };
+
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
 
+
+const addUser = (user) => {
+    users["users_list"].push(user)
+    return user
+}
+
+const deleteUserByName = (name) => {
+    users["users_list"] = users["users_list"].filter((user) => user["name"] !== name)
+}
+
+const deleteUserById = (id) => {
+    users["users_list"] = users["users_list"].filter((user) => user["id"] !== id)
+}
+
 //REST API ROUTES
+//GET
 app.get("/", (req, res) => {
-  res.send("Hello World!!");
+  res.send("Hello World!");
 });
   
 app.get("/users", (req, res) => {
 const name = req.query.name;
-if (name != undefined) {
-    let result = findUserByName(name);
+const job = req.query.job;
+
+if (name != undefined && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
     result = { users_list: result };
     res.send(result);
+} else if (name != undefined) {
+    let result = findUserByName(name)
+    result = { users_list: result }
+    res.send(result)
 } else {
     res.send(users);
 }
@@ -64,12 +91,34 @@ if (name != undefined) {
 app.get("/users/:id", (req,res) => {
     const id = req.params["id"]
     let result = findUserById(id)
+
     if(result){
         res.send(result)
     } else {
         res.status(404).send("Resource not found.")
     }
 })
+
+//POST
+app.post("/users", (req,res) => {
+    const userToAdd = req.body
+    addUser(userToAdd)
+    res.send()
+})
+
+//DELETE
+app.delete("/users", (req,res) => {
+    const user = req.body.name
+    deleteUserByName(user)
+    res.send()
+})
+
+app.delete("/users/:id", (req,res) => {
+    const id = req.params["id"]
+    deleteUserById(id)
+    res.send()
+})
+
 
 app.listen(port, () => {
   console.log(
